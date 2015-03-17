048d45e9b0b3ecd84c19ea34526fc9c68d5e6d12
dr.scottgriffiths@gmail.com <dr.scottgriffiths@gmail.com@44bf764c-bb7a-9818-b205-805bca3c8954>
2015-03-17 11:02:39 +0000

    Naming the background and some visual tweaks.

--- my-clock.js
+++ my-clock.js
@@ -52,12 +52,12 @@ tips_of_the_day.push("For extra accuracy when timing sporting events, use the vi
 tips_of_the_day.push("Use the Go Clock on an iPhone sellotaped to your wrist and your friend(s) will think you have an Apple Watch!");
 var current_tip;
 
-var backgrounds = ['wood1.jpg', 'wood2.jpg', 'stone1.jpg', 'mosaic1.jpg'];
+var backgrounds = [['wood1.jpg', "Dark wood"], ['wood2.jpg', "Light wood"], ['stone1.jpg', "Stone"], ['mosaic1.jpg', "Mosaic"]];
 var backgroundImages = [];
 
-var views = {0 : 'analogue', 1 : 'jump hour', 2 : 'digital', 3 : 'hybrid'};
+var views = {0 : 'Analogue', 1 : 'Jumping hour', 2 : 'Digital', 3 : 'Hybrid'};
 
-var stone_speeds = [['torpid', 2], ['slow', 5], ['normal', 9], ['fast', 18], ['insane!', 50]];
+var stone_speeds = [['Torpid', 2], ['Slow', 5], ['Normal', 9], ['Fast', 18], ['Insane!', 50]];
 
 $(document).ready(function(){
     var now = new Date();
@@ -66,10 +66,9 @@ $(document).ready(function(){
     time |= 0;
     var current_tip = time % tips_of_the_day.length;
     $('#tip_of_the_day').html(tips_of_the_day[current_tip]);
-//    $.slidebars();
     for (var i = 0; i < backgrounds.length; ++i) {
         backgroundImages.push(new Image());
-        backgroundImages[i].src = 'images/' + backgrounds[i];
+        backgroundImages[i].src = 'images/' + backgrounds[i][0];
     }
 });
 
@@ -89,8 +88,6 @@ $(window).load(function() {
     if (isInt(storedSpeed)) {
         stone_speed = parseInt(storedSpeed) % stone_speeds.length;
     }
-    goClock.speed = stone_speeds[stone_speed][1];
-    $('#stone_speed').text('Speed: '+ stone_speeds[stone_speed][0]);
 
     var view = 0;
     var storedView = readCookie('goban_view');
@@ -98,13 +95,31 @@ $(window).load(function() {
         view = parseInt(storedView) % 4;
     }
 
+    function setClockSpeed(s) {
+        s %= stone_speeds.length;
+        goClock.speed = stone_speeds[s][1];
+        $('#stone_speed').text(stone_speeds[s][0]);
+        createCookie('stone_speed', goClock.speed, 100);
+    }
+
     function setView(v) {
         v %= 4;
         goClock.view = v;
-        $('#clock_face').text('Clock face: ' + views[v]);
+        $('#clock_face').text(views[v]);
         createCookie('goban_view', goClock.view, 100);
     }
+
+    function setBackground(b) {
+        b %= backgrounds.length;
+        goClock.backgroundImage = backgroundImages[b];
+        goClock.draw(window.innerWidth, window.innerHeight - min_bottom_padding);
+        $('#change_background').text(backgrounds[b][1]);
+        createCookie('goban_background', goClock.background, 100);
+    }
+
+    setClockSpeed(stone_speed);
     setView(view);
+    setBackground(background);
 
     var fade_menu_timer;
     $('#about_box, #goban').click(function() {
@@ -132,29 +147,17 @@ $(window).load(function() {
         goClock.update();
     });
 
-    $('#next_tip').click(function() {
-        current_tip += 1;
-        current_tip %= tips_of_the_day.length;
-        $('#tip_of_the_day').html(tips_of_the_day[current_tip]);
-    });
-
-    $('#clock_face').click(function() {
+    $('#clock_face').closest('a').click(function() {
         setView(goClock.view + 1);
     });
 
-    $('#change_background').click(function() {
+    $('#change_background').closest('a').click(function() {
         background += 1;
-        background %= backgrounds.length;
-        goClock.backgroundImage = backgroundImages[background];
-        goClock.draw(window.innerWidth, window.innerHeight - min_bottom_padding);
-        createCookie('goban_background', goClock.background, 100);
+        setBackground(background);
     });
-    $('#stone_speed').click(function() {
+    $('#stone_speed').closest('a').click(function() {
         stone_speed += 1;
-        stone_speed %= stone_speeds.length;
-        goClock.speed = stone_speeds[stone_speed][1];
-        $('#stone_speed').text('Speed: '+ stone_speeds[stone_speed][0]);
-        createCookie('stone_speed', goClock.speed, 100);
+        setClockSpeed(stone_speed);
     });
 
     $('#about').click(function() {
